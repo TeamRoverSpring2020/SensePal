c132110ac33ed4556648278e2afaa1724f403c88
ThaliaR <thalia.rebecca@gmail.com>
2020-03-18 13:42:40 +0100

    added 1 test to get user from database

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void shouldFindUsernameByID() {
+        User user = userRepository.findById(1).get();
+
+        Assertions.assertEquals("Admin", user.getUsername());
     }
 
 }
