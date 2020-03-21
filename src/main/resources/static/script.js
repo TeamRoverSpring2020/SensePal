ffe39ba4b0887d207648d02573004ff5ecf6e88f
Oystein-FS <oystein.synnestvedt@gmail.com>
2020-03-21 13:21:15 +0100

    added js-method to control robot

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,14 @@
-window.onload = function test() {
-    console.log("test")
+
+function controlBoard(direction) {
+    const options = {
+        method: "POST",
+        body: JSON.stringify({direction: direction}),
+        headers: new Headers({
+            "Content-Type": "application/json"
+        })
+    }
+
+    fetch('http://localhost:8080/api/navigation', options)
+        .then(response => console.log(response));
 }
+
