efc7ace443f10fea389b9bdc1abec27c9dce7759
ChrisCross007 <58778210+ChrisCross007@users.noreply.github.com>
2020-03-23 15:26:05 +0100

    Added logout

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
-
+import com.example.demo.User;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String homePage() {
+    public String homePage(@ModelAttribute User user) {
         return "home";
     }
+    @GetMapping("/logout")
+    public String logout(){
+        return "login";
+    }
+
 }
--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
@@ -18,6 +22,8 @@ public class LoginController {
         model.addAttribute("user", new User());
         return "login";
     }
+
+
 
 
 }
