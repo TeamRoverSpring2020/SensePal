cf5a86711ff7faf3e06ee4ff8cfa357022f3e58c
ChrisCross007 <58778210+ChrisCross007@users.noreply.github.com>
2020-03-25 12:36:10 +0100

    merge

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/*@RestController
+@RestController
 public class ApiController {
 
-    /*@Autowired
+    @Autowired
     SensorObservationRepository sensorObservationRepository;
 
     @Autowired
@@ -29,4 +29,4 @@ public class ApiController {
         System.out.println(navigation);
     }
 
-} */
+}
