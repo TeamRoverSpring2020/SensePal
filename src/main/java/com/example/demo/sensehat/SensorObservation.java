9942309538736c41a51d6e1ddbb157cc9c85e5a2
Oystein-FS <oystein.synnestvedt@gmail.com>
2020-03-26 09:22:21 +0100

    some adjustements to the rasbian interface

--- src/main/java/com/example/demo/sensehat/SensorObservation.java
+++ src/main/java/com/example/demo/sensehat/SensorObservation.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 public class SensorObservation {
@@ -12,32 +13,51 @@ public class SensorObservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    //private static LocalDateTime dateTime;
+    private LocalDateTime timestamp;
     private Double temperature;
     private Double humidity;
     private Double pressure;
 
-    public SensorObservation(){
-    }
-
 /*    public SensorObservation(){
+    }*/
+
+    public SensorObservation(){
         SenseHatMethods senseHatMethods = new SenseHatMethods();
+        this.timestamp = LocalDateTime.now();
         this.temperature = senseHatMethods.getTemperature();
         this.humidity = senseHatMethods.getHumidity();
         this.pressure = senseHatMethods.getPressure();
-    }*/
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 
     public Double getTemperature() {
         return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
     }
 
     public Double getHumidity() {
         return humidity;
     }
 
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
+
     public Double getPressure() {
         return pressure;
     }
 
-
+    public void setPressure(Double pressure) {
+        this.pressure = pressure;
+    }
 }
