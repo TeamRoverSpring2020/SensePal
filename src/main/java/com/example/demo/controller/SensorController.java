c474cd91e1d8ec4a4fb805505e16bf9b1e5c6b11
ThaliaR <thalia.rebecca@gmail.com>
2020-03-26 14:45:34 +0100

    fixed login form

--- src/main/java/com/example/demo/controller/SensorController.java
+++ src/main/java/com/example/demo/controller/SensorController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.User;
+import com.example.demo.UserRepository;
 import com.example.demo.sensehat.SensorObservation;
 import com.example.demo.sensehat.SensorObservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +16,17 @@ public class SensorController {
     @Autowired
     private SensorObservationRepository sensorObservationRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
 
     @GetMapping("/sensordata")
-<<<<<<< HEAD
-    public String SensorDataPage(@ModelAttribute User user) {
-=======
     public String testPage(Model model) {
         SensorObservation sensorObservation = sensorObservationRepository.findById(2082).get();
         List<SensorObservation> sensorObservationList = (List<SensorObservation>) sensorObservationRepository.findAll();
         model.addAttribute(sensorObservation);
         model.addAttribute(sensorObservationList);
->>>>>>> 38ac7c79a5edc5ffc6732ca44d4c658c02f9fc5f
+
         return "sensordata";
     }
 
